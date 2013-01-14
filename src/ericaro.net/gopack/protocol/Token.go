1eecdd676d364b4e7fccac05de297bb7dd5418bc
Eric Atienza <eric@ericaro.net>
2013-01-14 08:57:41 +0100

    added Token generator

--- src/ericaro.net/gopack/protocol/Token.go
+++ src/ericaro.net/gopack/protocol/Token.go
@@ -2,11 +2,23 @@ package protocol
 
 import (
 	"encoding/base64"
+	"crypto/rand"
+	"io"
 )
 
 // Represent an identification Token
 // any []byte is ok. usually it is a fixed size []byte. 
 type Token []byte
+
+//NewToken generates a new random token of lengh length.
+func NewToken(length int) *Token {
+	b := make([]byte, length)
+	n, err := io.ReadFull(rand.Reader, b)
+	if n != len(b) || err != nil {
+		return nil
+	}
+	return (*Token)(&b)
+}
 
 //FormatURL format this token in a string suitable for url encoding. Mainly it uses base64.URLEncoding instead of std one
 func (t *Token) FormatURL() string {
@@ -17,14 +29,16 @@ func (t *Token) FormatURL() string {
 func (t *Token) FormatStd() string {
 	return base64.StdEncoding.EncodeToString(*t)
 }
+
 //ParseURLToken read a Token in a string
-func ParseURLToken(v string) (t  *Token, err error) {
+func ParseURLToken(v string) (t *Token, err error) {
 	b, err := base64.URLEncoding.DecodeString(v)
 	token := Token(b)
 	return &token, err
 }
+
 //ParseStdToken read a Token in a string using base64.URLEncoding
-func ParseStdToken(v string) (t  *Token, err error) {
+func ParseStdToken(v string) (t *Token, err error) {
 	b, err := base64.StdEncoding.DecodeString(v)
 	token := Token(b)
 	return &token, err
